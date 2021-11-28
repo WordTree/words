2bf5699ebc8658983827a1696a541d3f4fcbbbd2
LisnX <53891402+LisnXy@users.noreply.github.com>
2021-11-28 11:08:26 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <div class="header"><Header /></div>
+    <div class="User">
+      <div class="word-target">
+        <text-card title="复习计划" :content="`${reviewWordCount} words`" />
+        <text-card
+          title="学习进度"
+          :content="`${learnedWordCount} / ${totalWordCount} words`"
+        />
+      </div>
+      <div id="user-greeting">
+        <User :url="userImageUrl" :userName="userName" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import TextCard from "@/components/TextCard.vue";
+import User from "@/components/User.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    TextCard,
+    User,
+  },
+  data() {
+    return {
+      reviewWordCount: 0,
+      learnedWordCount: 0,
+      totalWordCount: 0,
+      userImageUrl:
+        "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
+      userName: "Lxy",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  background-color: rgb(239, 242, 245);
+  height: 100vh;
+}
+
+.User {
+  display: flex;
+  flex-wrap: wrap-reverse;
+}
+
+#user-greeting {
+  flex:1;
+}
+</style>
